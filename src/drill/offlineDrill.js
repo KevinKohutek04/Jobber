d3932af4b9657c7e18024cd23c2e1bb5c6639cf2
KevinKohutek04 <kohutekkevin7@gmail.com>
2024-07-19 09:05:54 -0500

    buttons fixes

--- src/drill/offlineDrill.js
+++ src/drill/offlineDrill.js
@@ -34,13 +34,11 @@ class OfflineDrill extends Component {
                 <div className='offlineDrill-stats'>
                 
                 <div className='offlineDrill-cards-right-right'>
-                    <div>
-                        <button className='offlineDrill-cards-button' style={{ backgroundColor: 'rgb(246, 71, 92)' }}>Wrong</button>
-                    </div>
+                    <button className='offlineDrill-cards-button' style={{ backgroundColor: 'rgb(246, 71, 92)' }}>Wrong</button>
                 </div>
                 <div className='offlineDrill-cards-right-left'>
-                        <button className='offlineDrill-cards-button' style={{ backgroundColor: 'rgb(17, 152, 102)' }}>Correct</button>
-                    </div>
+                    <button class="btn btn-layered-3d btn-layered-3d--pink">Correct</button>
+                </div>
                 </div>  {/*i take uo the bottom of the screen and do nothing*/}
             </div>
         );
@@ -49,3 +47,6 @@ class OfflineDrill extends Component {
 export default OfflineDrill;
 //<img src={arrowR} alt='right arrow' className='offlineDrill-cards-img' /> :(((
 //<img src={arrowL} alt='left arrow' className='offlineDrill-cards-img' />
+
+//<button className='offlineDrill-cards-button' style={{ backgroundColor: 'rgb(17, 152, 102)' }}>Correct</button>
+//box-shadow: rgb(17, 152, 102) 0px 0px 0px 2px, rgb(17, 152, 102) 0px 0.625em 0px 0px;
