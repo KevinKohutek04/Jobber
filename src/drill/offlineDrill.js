58492fb02cc0e466bdd11f9fa480a5a5abf6eeff
KevinKohutek04 <kohutekkevin7@gmail.com>
2024-07-19 08:44:31 -0500

    added buttonss

--- src/drill/offlineDrill.js
+++ src/drill/offlineDrill.js
@@ -34,18 +34,18 @@ class OfflineDrill extends Component {
                 <div className='offlineDrill-stats'>
                 
                 <div className='offlineDrill-cards-right-right'>
-                        <button className='button-56'>skbbidi</button>
-                        </div>
-                        <div className='offlineDrill-cards-right-left'>
-
-                        </div>
-                
+                    <div>
+                        <button className='offlineDrill-cards-button' style={{ backgroundColor: 'rgb(246, 71, 92)' }}>Wrong</button>
+                    </div>
+                </div>
+                <div className='offlineDrill-cards-right-left'>
+                        <button className='offlineDrill-cards-button' style={{ backgroundColor: 'rgb(17, 152, 102)' }}>Correct</button>
+                    </div>
                 </div>  {/*i take uo the bottom of the screen and do nothing*/}
             </div>
         );
     }
 }
-
 export default OfflineDrill;
 //<img src={arrowR} alt='right arrow' className='offlineDrill-cards-img' /> :(((
 //<img src={arrowL} alt='left arrow' className='offlineDrill-cards-img' />
